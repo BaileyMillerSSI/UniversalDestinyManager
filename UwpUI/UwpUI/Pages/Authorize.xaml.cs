8de2c80d5ffe28f19ec9708e950133e4883a9532
Bailey Miller <bmiller@susquehannasoftware.com>
2017-09-20 15:39:37 -0400

    Lots of blah code.

---
+++ UwpUI/UwpUI/Pages/Authorize.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UwpUI.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Authorize : Page
+    {
+        public Authorize()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LaunchAuthenticationBrowser();
+        }
+
+
+        private async void LaunchAuthenticationBrowser()
+        {
+            const string AuthorizationPath = "https://www.bungie.net/en/OAuth/Authorize?client_id={0}&response_type=code";
+
+            await Launcher.LaunchUriAsync(new Uri(String.Format(AuthorizationPath, "21086")));
+        }
+    }
+}
